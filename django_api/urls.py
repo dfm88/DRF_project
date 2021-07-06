d33e5d3040ac7ea48f2ca7238f4df5752a8f68d1
dfm88 <diegof.university@gmail.com>
2021-07-06 20:52:45 +0200

    added VieSets

--- django_api/urls.py
+++ django_api/urls.py
@@ -1,13 +1,20 @@
 
 from django.contrib import admin
-from django.urls import path
-from test_app.views import Simple, SimpleGenericsUpdate, SimpleGenerics
+from django.urls import path, include
+# from test_app.views import Simple, SimpleGenericsUpdate, SimpleGenerics
+from test_app.views import SimpleViewSet
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('simple-viewset', SimpleViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simple/', Simple.as_view()),
-    path('simple/<int:id>', Simple.as_view()),
-    path('simple-generics', SimpleGenerics.as_view()),
-    path('simple-generics/<int:id>', SimpleGenericsUpdate.as_view()),
+    # path('simple/', Simple.as_view()),
+    # path('simple/<int:id>', Simple.as_view()),
+    # path('simple-generics', SimpleGenerics.as_view()),
+    # path('simple-generics/<int:id>', SimpleGenericsUpdate.as_view()),
+    path('', include(router.urls))
 
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -6,52 +6,55 @@ from django.http import JsonResponse
 from .models import TestModel
 from django.forms.models import model_to_dict
 from .serializers import SimpleSerializer
+from rest_framework import viewsets
 
-# Fa la GET ALL e la POST
 
-
-class SimpleGenerics(generics.ListCreateAPIView):
+class SimpleViewSet(viewsets.ModelViewSet):
     queryset = TestModel.objects.all()
     serializer_class = SimpleSerializer
 
-# Fa la PUT o PATCH
 
+# # Fa la GET ALL e la POST
+# class SimpleGenerics(generics.ListCreateAPIView):
+#     queryset = TestModel.objects.all()
+#     serializer_class = SimpleSerializer
 
-class SimpleGenericsUpdate(generics.ListCreateAPIView):
-    queryset = TestModel.objects.all()
-    serializer_class = SimpleSerializer
-    # altrimenti nella url devo passare il parametro di
-    # defaulti 'pk' invece di 'id
-    lookup_field = "id"
+# # Fa la PUT o PATCH
+# class SimpleGenericsUpdate(generics.ListCreateAPIView):
+#     queryset = TestModel.objects.all()
+#     serializer_class = SimpleSerializer
+#     # altrimenti nella url devo passare il parametro di
+#     # defaulti 'pk' invece di 'id
+#     lookup_field = "id"
 
 
 # metodo verbose per definire le API
-class Simple(APIView):
+# class Simple(APIView):
 
-    def post(self, request):
-        # Serializzo prima quanto ricevuto dal cliente nei FORM DATA
-        serializers = SimpleSerializer(data=request.data)
-        # notifico automaticamente il client in caso di errori di validazione nei dati ricevuti
-        serializers.is_valid(raise_exception=True)
-        serializers.save()  # chiama il metodo Create del Serializer
-        return JsonResponse({"data": serializers.data})
+#     def post(self, request):
+#         # Serializzo prima quanto ricevuto dal cliente nei FORM DATA
+#         serializers = SimpleSerializer(data=request.data)
+#         # notifico automaticamente il client in caso di errori di validazione nei dati ricevuti
+#         serializers.is_valid(raise_exception=True)
+#         serializers.save()  # chiama il metodo Create del Serializer
+#         return JsonResponse({"data": serializers.data})
 
-    def get(self, request):
-        content = TestModel.objects.all()
-        return JsonResponse({'data': SimpleSerializer(content, many=True).data})
+#     def get(self, request):
+#         content = TestModel.objects.all()
+#         return JsonResponse({'data': SimpleSerializer(content, many=True).data})
 
-    def put(self, request, *args, **kwargs):
-        model_id = kwargs.get("id", None)
-        if not model_id:
-            return JsonResponse({"error": "method /PUT/ not alloed"})
+#     def put(self, request, *args, **kwargs):
+#         model_id = kwargs.get("id", None)
+#         if not model_id:
+#             return JsonResponse({"error": "method /PUT/ not alloed"})
 
-        try:
-            instance = TestModel.objects.get(id=model_id)
-        except:
-            return JsonResponse({"error": f"object with id '{model_id}' does not exists"})
-        # credo che passare l'instance sia ridondante, provato senza e funziona
-        serializers = SimpleSerializer(data=request.data, instance=instance)
-        # notifico automaticamente il client in caso di errori di validazione nei dati ricevuti
-        serializers.is_valid(raise_exception=True)
-        serializers.save()  # chiama il metodo Create del Serializer
-        return JsonResponse({"data": serializers.data})
+#         try:
+#             instance = TestModel.objects.get(id=model_id)
+#         except:
+#             return JsonResponse({"error": f"object with id '{model_id}' does not exists"})
+#         # credo che passare l'instance sia ridondante, provato senza e funziona
+#         serializers = SimpleSerializer(data=request.data, instance=instance)
+#         # notifico automaticamente il client in caso di errori di validazione nei dati ricevuti
+#         serializers.is_valid(raise_exception=True)
+#         serializers.save()  # chiama il metodo Create del Serializer
+#         return JsonResponse({"data": serializers.data})
